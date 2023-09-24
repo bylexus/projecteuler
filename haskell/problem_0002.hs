42d5904702d2b5a8209a6ecaa827af1242b339e5
Alexander Schenkel <aschenkel@kadenpartner.ch>
2023-09-24 20:05:41 +0200

    Small refactoring of fibonacchi

--- haskell/problem_0002.hs
+++ haskell/problem_0002.hs
@@ -7,14 +7,15 @@ fiboTo max
   | max < 1 = []
   | max == 1 = [1]
   | max == 2 = [1, 2]
-  | max == 3 = [1, 2, 3]
-  | max > 3 = reverse (_fiboTo [3, 2, 1] max)
+  | max > 2 = reverse (_fiboTo [2, 1] max)
 
 -- | internal Helper function to fiboTo:
 --   generates a list of fibonacchi elements up to a certain max value,
---   by appending to the front. MUST be started with [3,2,1].
+--   by appending to the front. MUST be started with an empty list
 _fiboTo :: [Integer] -> Integer -> [Integer]
 _fiboTo lst max = case lst of
+  [] -> _fiboTo [2, 1] max
+  [_] -> _fiboTo [2, 1] max
   (a : b : xs) ->
     if a + b < max
       then _fiboTo (a + b : lst) max
