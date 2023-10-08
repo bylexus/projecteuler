3abd9ad00ffe6a2b7362abdffcc13fe665750bd0
Alexander Schenkel <aschenkel@kadenpartner.ch>
2023-10-08 17:56:38 +0200

    Solution 9: Nicer solution with list comprehensions

--- haskell/problem_0009.hs
+++ haskell/problem_0009.hs
@@ -7,18 +7,16 @@
 
 import EulerLib qualified as E
 
-uplist2 a aMax = map (\x -> (a, x)) [a + 1 .. aMax]
-
-uplist3 (a, b) aMax = map (\x -> (a, b, x)) [b + 1 .. aMax]
-
 main = do
   -- Idea: form a tuple list (a, b, c) for all possible combinations,
   -- then filter by valid combinations (a^2 + b^2 = c^2)
   -- then filter by a+b+c = 1000
   let eulerProblem = 9
   let maxNr = 1000
-  let prod2list = foldr (\x a -> uplist2 x (maxNr - 1) ++ a) [] [2 .. maxNr - 2]
-  let prod3list = foldr (\x a -> uplist3 x maxNr ++ a) [] prod2list
-  let validProds = filter (\(a, b, c) -> a * a + b * b == c * c && a + b + c == 1000) prod3list
+  -- Build cartesian product of a,b,c, for each number a < b < c:
+  -- using a list comprehension:
+  let abc = [(a, b, c) | a <- [2 .. maxNr - 2], b <- [a + 1 .. maxNr - 1], c <- [b + 1 .. maxNr]]
+  let validProds = filter (\(a, b, c) -> a * a + b * b == c * c && a + b + c == 1000) abc
+
   let solution = head $ map (\(a, b, c) -> a * b * c) validProds
   E.printProblemSolution eulerProblem (show solution ++ " for the tuple " ++ show (head validProds))
