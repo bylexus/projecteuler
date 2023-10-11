f9d7f82d409b2dd7cef2bb9f0f1cd81a825acb02
Alexander Schenkel <aschenkel@kadenpartner.ch>
2023-10-11 20:13:53 +0200

    Problem 18 and therefore also Problem 67: solved with memoization. Much much MUCH faster.

--- haskell/problem_0067.hs
+++ haskell/problem_0067.hs
@@ -1,31 +1,14 @@
 {--------------------------------------------------------------------
 <p>By starting at the top of the triangle below and moving to adjacent numbers on the row below, the maximum total from top to bottom is 23.</p>
-<p class="monospace center"><span class="red"><b>3</b></span><br>
-<span class="red"><b>7</b></span> 4<br>
+<p class="monospace center"><span class="red"><b>3</b></span><br><span class="red"><b>7</b></span> 4<br>
 2 <span class="red"><b>4</b></span> 6<br>
 8 5 <span class="red"><b>9</b></span> 3</p>
 <p>That is, 3 + 7 + 4 + 9 = 23.</p>
-<p>Find the maximum total from top to bottom of the triangle below:</p>
-<p class="monospace center">75<br>
-95 64<br>
-17 47 82<br>
-18 35 87 10<br>
-20 04 82 47 65<br>
-19 01 23 75 03 34<br>
-88 02 77 73 07 63 67<br>
-99 65 04 28 06 16 70 92<br>
-41 41 26 56 83 40 80 70 33<br>
-41 48 72 33 47 32 37 16 94 29<br>
-53 71 44 65 25 43 91 52 97 51 14<br>
-70 11 33 28 77 73 17 78 39 68 17 57<br>
-91 71 52 38 17 14 91 43 58 50 27 29 48<br>
-63 66 04 68 89 53 67 30 73 16 69 87 40 31<br>
-04 62 98 27 23 09 70 98 73 93 38 53 60 04 23</p>
-<p class="note"><b>NOTE:</b> As there are only 16384 routes, it is possible to solve this problem by trying every route. However, <a href="problem=67">Problem 67</a>, is the same challenge with a triangle containing one-hundred rows; it cannot be solved by brute force, and requires a clever method! ;o)</p>
-
+<p>Find the maximum total from top to bottom in <a href="resources/documents/0067_triangle.txt">triangle.txt</a> (right click and 'Save Link/Target As...'), a 15K text file containing a triangle with one-hundred rows.</p>
+<p class="smaller"><b>NOTE:</b> This is a much more difficult version of <a href="problem=18">Problem 18</a>. It is not possible to try every route to solve this problem, as there are 2<sup>99</sup> altogether! If you could check one trillion (10<sup>12</sup>) routes every second it would take over twenty billion years to check them all. There is an efficient algorithm to solve it. ;o)</p>
 ---------------------------------------------------------------------}
 
-import Data.Map.Strict qualified as Map
+import Data.Map qualified as Map
 import Data.Maybe (fromJust)
 import EulerLib qualified as E
 
@@ -52,10 +35,9 @@ maxSumMem mMap (y, x) valueMap = (actValue, retMem)
     (rightVal, mem2) = maxSumMem mem1 (y + 1, x + 1) valueMap
 
 main = do
-  input <- readFile "data_0018.txt"
-  let eulerProblem = 18
+  input <- readFile "data_0067.txt"
+  let eulerProblem = 67
       rawValues = (map lineToValues . filter (/= "") . lines) input
       inputMap = E.listToCoordMap2 rawValues
-      -- solution = maxSum (0, 0) inputMap
       (solution, _) = maxSumMem Map.empty (0, 0) inputMap
   E.printProblemSolution eulerProblem solution
